400ee72a88b9fd0125bbe1b73c55f6efe0b4eef6
Flavio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2024-01-04 01:02:24 -0500

    Ruff Start (#6)
    * Ruff Start
    
    * Update project configuration and file structure

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,10 +3,10 @@
 # SPDX-License-Identifier: MIT
 from __future__ import annotations
 
-from typedfzf import fzf
+from fzf import fzf
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     s1: list[int] = fzf([1], multi=True)
     s2: int | None = fzf([2], multi=False)
     s3: int | None = fzf(range(100))
