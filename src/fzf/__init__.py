1131390884b2cc3d8e65f007b359ecf6b6ff8747
Flavio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2024-03-17 00:13:29 -0400

    speed up import time (#10)

--- src/fzf/__init__.py
+++ src/fzf/__init__.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
 
 try:
-    from setuptools_scm import get_version
-
-    __version__ = get_version(root="..", relative_to=__file__)
-except (ImportError, LookupError):
+    from fzf._version import (  # type: ignore[no-redef,unused-ignore]
+        __version__,  #
+    )
+except ModuleNotFoundError:
     try:
-        from fzf._version import (  # type: ignore[no-redef,unused-ignore] # noqa: F401
-            __version__,  #
-        )
-    except ModuleNotFoundError:
+        from setuptools_scm import get_version
+
+        __version__ = get_version(root="..", relative_to=__file__)
+    except (ImportError, LookupError):
         msg = "typedfzf is not correctly installed. Please install it with pip."
-        raise RuntimeError(msg)  # noqa: B904, TRY200
+        raise RuntimeError(msg) from None
 
 from fzf._fzf import *  # noqa: F403
